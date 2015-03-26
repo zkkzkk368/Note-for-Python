f85732fc56834ba18f602000f760094c24e157ba
zkkzkk368 <3140100805@zju.edu.cn>
2015-03-26 18:29:25 +0800

    chap1-10

---
+++ chap10 自带电池/hello2.py
@@ -0,0 +1,5 @@
+def hello()                              #  1
+    print 'hello,world!'                 #  2
+def test()                               #  3
+    hello()                              #  4
+if __name__ == '__main__': test()        #  5
